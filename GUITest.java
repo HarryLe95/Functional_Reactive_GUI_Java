96852da9b05333861d3032e8cef9cb60748e359e
HoangLe <hoang.le@consilium.technology>
2022-11-03 18:45:49 +1030

    Add feature for part 3

--- GUITest.java
+++ GUITest.java
@@ -15,33 +15,37 @@ public class GUITest extends GUIFinal {
 
     /**
      * GUI Test constructors
-     * @param name - name of the frame
+     *
+     * @param name       - name of the frame
      * @param windowSize - window dimension
      * @param numStreams - number of display streams to generate data
-     * @param dtFactor - controls each event stream fire rate. Each stream is fired at regular interval of (id+1)*dtFactor
-     * @param tClear - time period to retain event data in memory to calculate distance travelled. 5 mins or 300s in the
-     *               official GUI but reduced to 5s for testing purposes.
+     * @param dtFactor   - controls each event stream fire rate. Each stream is fired at regular interval of (id+1)*dtFactor
+     * @param tClear     - time period to retain event data in memory to calculate distance travelled. 5 mins or 300s in the
+     *                   official GUI but reduced to 5s for testing purposes.
      */
     public GUITest(String name, Dimension windowSize,
                    int numStreams, double dtFactor, double tClear) {
         super(name, windowSize);
         this.tClear = tClear;
         this.streams = new Stream[numStreams];
-        for (int index = 0; index < numStreams; index++) {
+        for (int index = 0; index < numStreams - 1; index++) {
             streams[index] = generateEvent(Integer.toString(index),
-                    index*10, index*10,(index + 1)*dtFactor);
+                    index * 10, index * 10, (index + 1) * dtFactor, false);
         }
+        streams[numStreams - 1] = generateEvent(Integer.toString(numStreams - 1), 0,0,
+                1, true);
     }
 
     /**
      * Generate GpsEvent stream based on input data
+     *
      * @param name - event id
-     * @param lat - event lat
-     * @param lon - event long
-     * @param dt - event firing interval
+     * @param lat  - event lat
+     * @param lon  - event long
+     * @param dt   - event firing interval
      * @return a stream of GpsEvent.
      */
-    public Stream<GpsEvent> generateEvent(String name, double lat, double lon, double dt) {
+    public Stream<GpsEvent> generateEvent(String name, double lat, double lon, double dt, boolean changeLoc) {
         Stream<GpsEvent> event = Transaction.run(
                 () -> {
                     CellLoop<Double> cTime = new CellLoop<>();
@@ -53,13 +57,17 @@ public class GUITest extends GUIFinal {
                                     }
                             ));
                     cTime.loop(sGen.map(u -> time.sample()).hold(0.0));
-                    return sGen.map(u -> new GpsEvent(name, lat, lon, time.sample()*3.281));
+                    if (!changeLoc) {
+                        return sGen.map(u -> new GpsEvent(name, lat, lon, time.sample() * 3.281));
+                    }
+                    return sGen.map(u -> new GpsEvent(name, lat, Math.floorMod((int) Math.floor(time.sample()), 2) * 100, 0));
+
                 });
         return event;
     }
 
     public static void main(String[] args) {
-        GUITest GUI = new GUITest("GpsTracker", new Dimension(850, 600),10,0.5,5);
+        GUITest GUI = new GUITest("GpsTracker", new Dimension(850, 600), 10, 0.5, 5);
         GPanel pMain = new GPanel(BoxLayout.PAGE_AXIS);
 
         Transaction.runVoid(
